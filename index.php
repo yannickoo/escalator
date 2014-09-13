40ba6443042aa96c3be12e00f270e6548f7e69d4
yannickoo <drupal@yannickoo.de>
2014-09-13 22:53:21 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Rolltreppe</title>
+    <style>
+      body {
+        margin: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <video src="rolltreppe.webm" style="width: 100%;" loop autoplay></video>
+  </body>
+</html>
